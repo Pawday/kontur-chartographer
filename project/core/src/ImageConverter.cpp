2ad527be2dc8dbac062a38f4f4eb45dca9b86e5b
Pawday <pawday@mail.ru>
2022-03-10 00:21:07 +0300

    ImageConverter bmp padding optimization

--- project/core/src/ImageConverter.cpp
+++ project/core/src/ImageConverter.cpp
@@ -10,11 +10,12 @@ Charta::Bmp24RGB Charta::ImageConverter::RawToBmp(RawImage24& inputRawImage)
     Bmp24RGB ret(width, height);
 
     uint8_t* outputBMPPixelDataBuffer = ret.GetRawPixelData();
+    uint8_t bmpPaddingSize = (4 - ((width * 3) % 4));
     
     for (uint32_t y = 0; y < height; y++)
     {
-        size_t bmpPadding = (4 - ((width * 3) % 4)) * (height - 1 - y);
-        if ((width * 3) % 4 == 0) bmpPadding = 0;
+        size_t currentPaddingLevel = bmpPaddingSize * (height - 1 - y);
+        if ((width * 3) % 4 == 0) currentPaddingLevel = 0;
 
         for (uint32_t x = 0; x < width; x++)
         {
@@ -27,9 +28,9 @@ Charta::Bmp24RGB Charta::ImageConverter::RawToBmp(RawImage24& inputRawImage)
             uint8_t inputGreen = inputRawImageDataBuffer[rawImagePixelPos * 3 + 1];
             uint8_t inputBlue = inputRawImageDataBuffer[rawImagePixelPos * 3 + 2];
 
-            outputBMPPixelDataBuffer[bmpImagePixelPos * 3 + 0 + bmpPadding] = inputBlue;
-            outputBMPPixelDataBuffer[bmpImagePixelPos * 3 + 1 + bmpPadding] = inputGreen;
-            outputBMPPixelDataBuffer[bmpImagePixelPos * 3 + 2 + bmpPadding] = inputRed;
+            outputBMPPixelDataBuffer[bmpImagePixelPos * 3 + 0 + currentPaddingLevel] = inputBlue;
+            outputBMPPixelDataBuffer[bmpImagePixelPos * 3 + 1 + currentPaddingLevel] = inputGreen;
+            outputBMPPixelDataBuffer[bmpImagePixelPos * 3 + 2 + currentPaddingLevel] = inputRed;
         }
     }
     
@@ -47,20 +48,22 @@ Charta::RawImage24 Charta::ImageConverter::BmpToRaw(Bmp24RGB& inputBMPImage)
     RawImage24 ret(width, height);
 
     uint8_t* rawImageDataBuffer = ret.GetRawData();
-    
+
+    uint8_t bmpPaddingSize = (4 - ((width * 3) % 4));
+
     for (uint32_t y = 0; y < height; y++)
     {
-        size_t bmpPadding = (4 - ((width * 3) % 4)) * (height - 1 - y);
-        if ((width * 3) % 4 == 0) bmpPadding = 0;
+        size_t currentPaddingLevel = bmpPaddingSize * (height - 1 - y);
+        if ((width * 3) % 4 == 0) currentPaddingLevel = 0;
 
         for (uint32_t x = 0; x < width; x++)
         {
             size_t rawImagePixelPos = (y * width + x);
             size_t bmpImagePixelPos = ((height - 1 - y) * width + x);
 
-            uint8_t inputBlue = inputBMPPixelDataBuffer[bmpImagePixelPos * 3 + 0 + bmpPadding];
-            uint8_t inputGreen = inputBMPPixelDataBuffer[bmpImagePixelPos * 3 + 1 + bmpPadding];
-            uint8_t inputRed = inputBMPPixelDataBuffer[bmpImagePixelPos * 3 + 2 + bmpPadding];
+            uint8_t inputBlue = inputBMPPixelDataBuffer[bmpImagePixelPos * 3 + 0 + currentPaddingLevel];
+            uint8_t inputGreen = inputBMPPixelDataBuffer[bmpImagePixelPos * 3 + 1 + currentPaddingLevel];
+            uint8_t inputRed = inputBMPPixelDataBuffer[bmpImagePixelPos * 3 + 2 + currentPaddingLevel];
 
             rawImageDataBuffer[rawImagePixelPos * 3 + 0] = inputRed;
             rawImageDataBuffer[rawImagePixelPos * 3 + 1] = inputGreen;
